8c755cc3b8514fc714d6f8d4c7edfb7d0783d9c1
rafaelfontes777 <rafaelfontes777@hotmail.com>
2021-08-26 11:51:42 -0300

    Category

--- src/main/java/com/example/aula_spring/config/TestConfig.java
+++ src/main/java/com/example/aula_spring/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.example.aula_spring.config;
 
+import com.example.aula_spring.entities.Category;
 import com.example.aula_spring.entities.Order;
 import com.example.aula_spring.entities.User;
 import com.example.aula_spring.entities.enums.OrderStatus;
+import com.example.aula_spring.repositories.CategoryRepository;
 import com.example.aula_spring.repositories.OrderRepository;
 import com.example.aula_spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -36,6 +41,12 @@ public class TestConfig implements CommandLineRunner {
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 
     }
--- src/main/java/com/example/aula_spring/entities/Order.java
+++ src/main/java/com/example/aula_spring/entities/Order.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
